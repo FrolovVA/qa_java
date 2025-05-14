7fe7e171ef1dc105c09245d3fff3601d1be04a1f
Vladimir Frolov <frolovva11@yandex.ru>
2025-05-14 15:55:27 +0300

    add jacoco report

---
+++ src/test/java/com/example/FelineTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import junit.framework.TestCase;
+
+public class FelineTest extends TestCase {
+
+    public void testEatMeat() {
+    }
+
+    public void testGetFamily() {
+    }
+
+    public void testGetKittens() {
+    }
+
+    public void testTestGetKittens() {
+    }
+}
---
+++ src/test/java/com/example/LionTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import junit.framework.TestCase;
+
+public class LionTest extends TestCase {
+
+    public void testGetKittens() {
+    }
+
+    public void testDoesHaveMane() {
+    }
+
+    public void testGetFood() {
+    }
+}
